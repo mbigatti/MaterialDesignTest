7a271158a4e22dc959c8ad0a03ce84914d3ae893
mbigatti <massimiliano@bigatti.it>
2014-07-01 14:24:02 +0200

    Initial Commit

---
+++ MaterialDesignTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaterialDesignTest
+//
+//  Created by Massimiliano Bigatti on 01/07/14.
+//  Copyright (c) 2014 Massimiliano Bigatti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
